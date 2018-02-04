4c68ce19b96be1ef11bda7decf684ed5852ffc5a
Julien BAYLE <jbayle@gmail.com>
2018-02-04 22:53:02 +0000

    Fix catkin make install error with ptyhon package

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
         packages=['xv_11_laser_motor_control'],
-        package_dir={'': 'src'},
+        package_dir={'': 'scripts'},
         )
 setup(**setup_args)
